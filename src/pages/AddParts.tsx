a19b6ec3440008098f0ff281baec34f3783765ff
savvymfai <129531389+savvymfai@users.noreply.github.com>
2023-04-02 17:05:51 -0600

    jam on it

--- src/pages/AddParts.tsx
+++ src/pages/AddParts.tsx
@@ -9,6 +9,19 @@ interface CarPart {
   description: string;
 }
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAbgkEx5-XXkStn7IMHLTyQKxY-9GwWzd4",
+  authDomain: "car-parts-finder-88840.firebaseapp.com",
+  projectId: "car-parts-finder-88840",
+  storageBucket: "car-parts-finder-88840.appspot.com",
+  messagingSenderId: "771209660481",
+  appId: "1:771209660481:web:f3fa6bcd3f6bf008af1ec0"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 const AddRemoveCarPartsPage: React.FC = () => {
   const [carParts, setCarParts] = useState<CarPart[]>([]);
   const [newCarPart, setNewCarPart] = useState<CarPart>({ id: '', name: '', description: '' });
@@ -48,8 +61,7 @@ const AddRemoveCarPartsPage: React.FC = () => {
     const db = firebase.database();
     const carPartsRef = db.ref('carParts');
     const newCarPartRef = carPartsRef.push();
-    await newCarPartRef.set(newCarPart);
-    setCarParts((prevState) => [...prevState, { ...newCarPart, id: newCarPartRef.key! }]);
+    await newCarPartRef.set(newCarPart);\n    setCarParts((prevState) => [...prevState, { ...newCarPart, id: newCarPartRef.key! }]);
     setNewCarPart({ id: '', name: '', description: '' });
   };
 
