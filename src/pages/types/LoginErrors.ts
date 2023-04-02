461ad60c648d1bbfcde51b31b51e3a5be8e71164
savvymfai <129531389+savvymfai@users.noreply.github.com>
2023-04-02 15:45:21 -0600

    LFG

--- src/pages/types/LoginErrors.ts
+++ src/pages/types/LoginErrors.ts
@@ -1,6 +1,15 @@
+import React from 'react';
+
 interface LoginError {
-    message: string;
-  }
-  
-  export default LoginError;
-  
+  message: string;
+}
+
+const LoginErrors: React.FC<{ errors: LoginError[] }> = ({ errors }) => (
+  <>
+    {errors.map((error) => (
+      <React.Fragment key={error.message}>{error.message}</React.Fragment>
+    ))}
+  </>
+);
+
+export default LoginErrors;
