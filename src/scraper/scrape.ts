3070dc26cd02f0603a5e6478f48ff180645d62e7
savvymfai <129531389+savvymfai@users.noreply.github.com>
2023-04-02 14:58:10 -0600

    Update scrape.ts
    kick push data scraper

--- src/scraper/scrape.ts
+++ src/scraper/scrape.ts
@@ -1,17 +1,19 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+import { CheerioElement } from 'cheerio';
+
 
 interface CarPart {
   name: string;
   price: number;
+  url?: string;
 }
 
 async function fetchAutoZone(query: string): Promise<CarPart[]> {
   const response = await axios.get(`https://www.autozone.com/search?q=${query}`);
   const $ = cheerio.load(response.data);
 
-  // Update these selectors based on the actual structure of the AutoZone website
-  const parts = $('.part-selector').map((_, element) => {
+  const parts: CarPart[] = $('.part-selector').map((_: number, element: CheerioElement) => {
     const name = $(element).find('.part-name').text().trim();
     const priceText = $(element).find('.part-price').text().replace(/[$,]/g, '').trim();
     const price = parseFloat(priceText);
@@ -19,15 +21,14 @@ async function fetchAutoZone(query: string): Promise<CarPart[]> {
     return { name, price };
   }).toArray();
 
-  return parts as CarPart[];
+  return parts;
 }
 
 async function fetchOReilly(query: string): Promise<CarPart[]> {
   const response = await axios.get(`https://www.oreillyauto.com/search?q=${query}`);
   const $ = cheerio.load(response.data);
 
-  // Update these selectors based on the actual structure of the O'Reilly Auto Parts website
-  const parts = $('.part-selector').map((_, element) => {
+  const parts: CarPart[] = $('.part-selector').map((_: number, element: CheerioElement) => {
     const name = $(element).find('.part-name').text().trim();
     const priceText = $(element).find('.part-price').text().replace(/[$,]/g, '').trim();
     const price = parseFloat(priceText);
@@ -35,12 +36,11 @@ async function fetchOReilly(query: string): Promise<CarPart[]> {
     return { name, price };
   }).toArray();
 
-  return parts as CarPart[];
+  return parts;
 }
 
 export async function comparePrices(query: string): Promise<{ autoZone: CarPart[]; oReilly: CarPart[] }> {
-  const autoZone = await fetchAutoZone(query);
-  const oReilly = await fetchOReilly(query);
+  const [autoZone, oReilly] = await Promise.all([fetchAutoZone(query), fetchOReilly(query)]);
 
   return { autoZone, oReilly };
 }
